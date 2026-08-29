10fc5ed48f3b141185fdfdf83194c1b036fb86c2
justynasznajder-dot <justyna.sznajder@gmail.com>
2026-08-29 10:18:04 +0200

    update project

--- app/layout.tsx
+++ app/layout.tsx
@@ -17,7 +17,8 @@ export const metadata: Metadata = {
   title: "Harry English - angielski z pasją",
   description: "Szkoła języka angielskiego. Zajęcia dla przedszkolaków, uczniów i dorosłych. Małe grupy, doświadczeni lektorzy.",
   icons: {
-    icon: "/icon.svg",
+    icon: [{ url: "/images/logo_link.png", type: "image/png", sizes: "1200x1200" }],
+    apple: [{ url: "/images/logo_link.png", type: "image/png", sizes: "1200x1200" }],
   },
   metadataBase: new URL("https://www.harry-english.pl"),
   openGraph: {
@@ -26,11 +27,20 @@ export const metadata: Metadata = {
     url: "https://www.harry-english.pl",
     siteName: "Harry English",
     type: "website",
+    images: [
+      {
+        url: "/images/logo_link.png",
+        width: 1200,
+        height: 1200,
+        alt: "Harry English",
+      },
+    ],
   },
   twitter: {
     card: "summary_large_image",
     title: "Harry English - angielski z pasją",
     description: "Szkoła języka angielskiego. Zajęcia dla przedszkolaków, uczniów i dorosłych. Małe grupy, doświadczeni lektorzy.",
+    images: ["/images/logo_link.png"],
   },
 };
 
